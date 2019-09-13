ef88ec1a2d8f9117f22147cf93194056fbfb5cda
Gwenaël Robert <gwenael.rob@gmail.com>
2019-09-13 15:59:14 +0200

    splimplify webpack files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,14 +20,7 @@ module.exports = {
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader'
-          },
-          {
-            loader: 'ts-loader'
-          }
-        ]
+        use: ['babel-loader', 'ts-loader']
       },
       {
         test: /\.(js|jsx)$/,
